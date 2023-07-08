a57989363ac7a100175f55ce3b2fbf536acd51a5
fahadalamjoy <61037426+fahadalamjoy@users.noreply.github.com>
2023-07-08 19:20:58 +0600

    chat

--- routes/messages.js
+++ routes/messages.js
@@ -4,11 +4,26 @@ const Message = require("../models/Message");
 //add
 
 router.post("/", async (req, res) => {
+  // const { conversationId, sender, receiver, text } = req.body;
+  // console.log(conversationId, sender, receiver, text)
   const newMessage = new Message(req.body);
 
   try {
     const savedMessage = await newMessage.save();
     res.status(200).json(savedMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get("/getMessage/:sender/:receiver", async (req, res) => {
+  try {
+    const messages = await Message.find({
+      sender: req.params.sender,
+      receiver: req.params.receiver
+    });
+    // console.log(messages)
+
+    res.status(200).json(messages);
   } catch (err) {
     res.status(500).json(err);
   }
